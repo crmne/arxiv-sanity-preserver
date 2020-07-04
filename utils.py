a698777876c46457d241524daef7e3c03dc8c446
Carmine Paolino <carmine@paolino.me>
2020-07-05 00:07:35 +0200

    formatted using autoflake, isort and yapf

--- utils.py
+++ utils.py
@@ -1,9 +1,9 @@
+import os
+import pickle
+import re
+import tempfile
 from contextlib import contextmanager
 
-import os
-import re
-import pickle
-import tempfile
 
 # global settings
 # -----------------------------------------------------------------------------
@@ -20,13 +20,14 @@ class Config(object):
     sim_path = 'sim_dict.p'
     user_sim_path = 'user_sim.p'
     # sql database file
-    db_serve_path = 'db2.p' # an enriched db.p with various preprocessing info
+    db_serve_path = 'db2.p'  # an enriched db.p with various preprocessing info
     database_path = 'as.db'
     serve_cache_path = 'serve_cache.p'
-    
-    beg_for_hosting_money = 1 # do we beg the active users randomly for money? 0 = no.
-    banned_path = 'banned.txt' # for twitter users who are banned
+
+    beg_for_hosting_money = 1  # do we beg the active users randomly for money? 0 = no.
+    banned_path = 'banned.txt'  # for twitter users who are banned
     tmp_dir = 'tmp'
+
 
 # Context managers for atomic writes courtesy of
 # http://stackoverflow.com/questions/2333872/atomic-writing-to-file-with-python
@@ -83,6 +84,7 @@ def open_atomic(filepath, *args, **kwargs):
                 os.fsync(f.fileno())
         os.rename(tmppath, filepath)
 
+
 def safe_pickle_dump(obj, fname):
     with open_atomic(fname, 'wb') as f:
         pickle.dump(obj, f, -1)
@@ -91,11 +93,13 @@ def safe_pickle_dump(obj, fname):
 # arxiv utils
 # -----------------------------------------------------------------------------
 
+
 def strip_version(idstr):
     """ identity function if arxiv id has no version, otherwise strips it. """
     parts = idstr.split('v')
     return parts[0]
 
+
 # "1511.08198v1" is an example of a valid arxiv id that we accept
 def isvalidid(pid):
-  return re.match('^\d+\.\d+(v\d+)?$', pid)
+    return re.match('^\d+\.\d+(v\d+)?$', pid)
